c8eaf7a0a16c1cbc3cf54943b0958c9046c183dd
David Francisco <dfrancisco2677@gmail.com>
2022-07-29 10:13:37 -0700

    Created a test that pulls all games, and created one that tests id 1 for the object in my database.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,24 +1,32 @@
-// const pool = require('../lib/utils/pool');
-// const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
-
-// const { games } = require; 
-
-// describe('games routes', () => {
-//   beforeEach(() => {
-//     return setup(pool);
-//   });
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
 
 
-//   it('/games should give all games in database', async () => {
-//     const res = await request(app).get('/games');
-//     const expected = games.map((game) => {
-//       return { id: game.id, name: game.name, genre: game.genre };
-//     });
-//     expect(res.body).toEqual(expected);
-//   });
-//   afterAll(() => {
-//     pool.end();
-//   });
-// });
+describe('games routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+
+  it('/games should give all games in database', async () => {
+    const resp = await request(app).get('/games');
+    expect(resp.body.length).toEqual(6);
+  });
+});
+
+it('/games/id should give a game specific to the id', async () => {
+  const resp = await request(app).get('/games/1');
+  expect(resp.body).toEqual({
+    id: '1',
+    name: 'Stray',
+    genre: 'Cat Simulator',
+    platform: 'Multiplatform',
+    review: 'If you want to be a cat, this has a dedicated meow button. 10/10',
+    yearReleased: 2022,
+  });
+});
+afterAll(() => {
+  pool.end();
+});
