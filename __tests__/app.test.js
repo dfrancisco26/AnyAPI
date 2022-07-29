3382d1759019dca6b1cb8071acfcc1a34f84e1fa
David Francisco <dfrancisco2677@gmail.com>
2022-07-29 07:43:57 -0700

    Created Game model and built out a test. Need to ask about tests to get them working.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,24 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
-const request = require('supertest');
-const app = require('../lib/app');
+// const pool = require('../lib/utils/pool');
+// const setup = require('../data/setup');
+// const request = require('supertest');
+// const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
-  afterAll(() => {
-    pool.end();
-  });
-});
+// const { games } = require; 
+
+// describe('games routes', () => {
+//   beforeEach(() => {
+//     return setup(pool);
+//   });
+
+
+//   it('/games should give all games in database', async () => {
+//     const res = await request(app).get('/games');
+//     const expected = games.map((game) => {
+//       return { id: game.id, name: game.name, genre: game.genre };
+//     });
+//     expect(res.body).toEqual(expected);
+//   });
+//   afterAll(() => {
+//     pool.end();
+//   });
+// });
